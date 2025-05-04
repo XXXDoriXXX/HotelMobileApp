860f0b880e0e0be0f3b04a213075f2319830ed34
Vadym <149205033+XXXDoriXXX@users.noreply.github.com>
2025-05-05 00:19:58 +0300

    add more adaptation, fix crashes

--- app/src/main/java/com/example/hotelapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/hotelapp/ui/MainActivity.kt
@@ -13,12 +13,14 @@ import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
@@ -27,6 +29,7 @@ import com.example.hotelapp.R
 import com.example.hotelapp.classes.ImageCacheProxy
 import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.delay
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -34,30 +37,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private val handler = Handler(Looper.getMainLooper())
     private lateinit var permissionCheckRunnable: Runnable
+    private var hasLaunchedPermissionCycle = false
+    private lateinit var permissionsLauncher: ActivityResultLauncher<Array<String>>
 
-    private val permissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        Log.d("Permissions", "Result: $permissions")
-
-        val locationGranted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
-        val galleryGranted = permissions[Manifest.permission.READ_MEDIA_IMAGES] == true
-
-        if (locationGranted && galleryGranted) {
-            handler.removeCallbacks(permissionCheckRunnable)
-            updateLocationOnce()
-        } else {
-            // повтор через 3 сек
-            handler.postDelayed(permissionCheckRunnable, 3000)
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         UserHolder.initialize(this)
         super.onCreate(savedInstanceState)
         ThemeManager.applyTheme(this)
         setContentView(R.layout.activity_main)
 
-        startPermissionCycle()
+        permissionsLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            val locationGranted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+            val galleryGranted = permissions[Manifest.permission.READ_MEDIA_IMAGES] == true
+
+            if (locationGranted && galleryGranted) {
+                updateLocationOnce()
+            } else {
+                lifecycleScope.launchWhenStarted {
+                    delay(3000)
+                    startPermissionCycle()
+                }
+            }
+        }
 
         setupSystemUI()
         setupInsets()
@@ -67,27 +70,37 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.setupWithNavController(navController)
 
         handleStartFragment(intent)
+
+        lifecycleScope.launchWhenStarted {
+            startPermissionCycle()
+        }
     }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        if (!hasLaunchedPermissionCycle) {
+            hasLaunchedPermissionCycle = true
+            startPermissionCycle()
+        }
+    }
+
 
     private fun startPermissionCycle() {
-        permissionCheckRunnable = object : Runnable {
-            override fun run() {
-                if (hasAllPermissions()) {
-                    updateLocationOnce()
-                } else {
-                    permissionLauncher.launch(
-                        arrayOf(
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION,
-                            Manifest.permission.READ_MEDIA_IMAGES
-                        )
-                    )
-                }
-            }
+        if (hasAllPermissions()) {
+            updateLocationOnce()
+        } else {
+            permissionsLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                )
+            )
         }
-
-        handler.post(permissionCheckRunnable)
     }
+
     private fun hasAllPermissions(): Boolean {
         return listOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -97,54 +110,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkAndRequestPermissions() {
-        val permissionsToRequest = mutableListOf<String>()
-
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
-            checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ) {
-            permissionsToRequest.add(Manifest.permission.ACCESS_FINE_LOCATION)
-            permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-        }
-
-        if (checkSelfPermission(Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
-            permissionsToRequest.add(Manifest.permission.READ_MEDIA_IMAGES)
-        }
-
-        if (permissionsToRequest.isNotEmpty()) {
-            requestPermissionsLauncher.launch(permissionsToRequest.toTypedArray())
-        } else {
-            updateLocationOnce()
-        }
-    }
-
-    private val requestPermissionsLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        val locationGranted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
-        val galleryGranted = permissions[Manifest.permission.READ_MEDIA_IMAGES] == true
-
-        if (locationGranted) updateLocationOnce()
-        if (galleryGranted) Log.d("GalleryAccess", "Доступ до фото надано")
-    }
-
-    private fun setupSystemUI() {
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        window.navigationBarColor = Color.TRANSPARENT
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
-    }
-
-    private fun setupInsets() {
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
     @SuppressLint("MissingPermission")
     private fun updateLocationOnce() {
         val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -167,7 +132,24 @@ class MainActivity : AppCompatActivity() {
                 Log.w("GeoUpdate", "Location is null")
             }
         }
+    }
 
+    private fun setupSystemUI() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        window.navigationBarColor = Color.TRANSPARENT
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
+
+    private fun setupInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
     private fun handleStartFragment(intent: Intent?) {
@@ -210,6 +192,5 @@ class MainActivity : AppCompatActivity() {
         intent?.removeExtra("startFragment")
         intent?.removeExtra("openBookingId")
     }
-
-
 }
+
